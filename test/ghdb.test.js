d1ba9277007aacea8333ad578705949146f6d9ae
Agustí Moll <agusti@gmail.com>
2020-10-31 23:31:34 +0100

    Create tests

--- test/ghdb.test.js
+++ test/ghdb.test.js
@@ -1,19 +1,28 @@
 const Ghdb = require("../src/ghdb");
 require('dotenv').config({debug: true});
+var ghdbObj
+
+beforeAll(()=>{
+    ghdbObj = new Ghdb( { personalAccessToken: process.env.ACCESSTOKEN, 
+        owner: process.env.GH_USER, 
+        repo: process.env.GH_REPOSITORY, 
+        path: process.env.GH_PATH } )
+})
 
 test("Create object", () => {
-    var ghdbObj = new Ghdb( { personalAccessToken: process.env.ACCESSTOKEN, 
-                              owner: process.env.GH_USER, 
-                              repo: process.env.GH_REPOSITORY, 
-                              path: process.env.GH_PATH } )
     expect(ghdbObj.repo).toBe(process.env.GH_REPOSITORY);
 });
 
 test("Write file", () => {
-    var ghdbObj = new Ghdb( { personalAccessToken: process.env.ACCESSTOKEN, 
-        owner: process.env.GH_USER, 
-        repo: process.env.GH_REPOSITORY, 
-        path: process.env.GH_PATH } )
-    //ghdbObj.toString()
-    ghdbObj.lowWriteGithub("hola.json", {hello: 'World'})
+    return ghdbObj.lowWriteGithub("hola.json", {hello: 'World'})
+    .then( data => {
+        expect(data.status).toBe(200)
+    })
+})
+
+test("Read file", () => {
+    return ghdbObj.lowReadGithub("hola.json")
+    .then( data => {
+        expect(data.content.hello).toBe('World')
+    })
 })
