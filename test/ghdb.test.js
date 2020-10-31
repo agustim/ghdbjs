8770d711647f6057b6d5f1d01857fb7f8ddf0911
Agustí Moll <agusti@gmail.com>
2020-10-31 20:37:06 +0100

    Add dotenv

--- test/ghdb.test.js
+++ test/ghdb.test.js
@@ -1,6 +1,10 @@
 const Ghdb = require("../src/ghdb");
+require('dotenv').config({debug: true});
 
 test("Create object", () => {
-    var ghdbObj = new Ghdb( { personalAccessToken:"pat", owner: "me", repo: "myrepo", path: "path" } )
-    expect(ghdbObj.repo).toBe("myrepo");
+    var ghdbObj = new Ghdb( { personalAccessToken: process.env.ACCESSTOKEN, 
+                              owner: process.env.USER, 
+                              repo: process.env.REPOSITORY, 
+                              path: process.env.PATH } )
+    expect(ghdbObj.repo).toBe("r");
 });
