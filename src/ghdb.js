c8a8114c2a2cda8bf9e3365f003cb74eb64ba57d
Agustí Moll <agusti@gmail.com>
2020-11-02 23:36:42 +0100

    Add SelfCategories for quickly system to get categories from object.

--- src/ghdb.js
+++ src/ghdb.js
@@ -9,7 +9,9 @@ function Ghdb ( config ) {
     this.path = config.path;
     this.octokit = null;
 
-    this.storage = "storage/" 
+    this.storage = "storage/"
+    this.category = "category/"
+    this.selfCategory = "self/"
     this.toString = function(){
         return JSON.stringify(this)
     }
@@ -38,19 +40,48 @@ function Ghdb ( config ) {
         if (!listCategories) {
             listCategories = []
         }
+        if (!Array.isArray(listCategories)) {
+            listCategories = [ listCategories ]
+        }
         // Write record
         const uuid = await this.generateUID()
-        this.lowWriteGithub(this.storage + uuid, obj)
+        await this.lowWriteGithub(this.storage + uuid, obj)
+        // Write in selfCategories
+        await this.lowWriteGithub(this.selfCategory + uuid, listCategories)
         // Write in categories
-
+        for await (let e of listCategories) {
+            await this.lowWriteGithub(this.category + e + "/" + uuid)
+        }
+        return uuid
+    }
+    this.read = async function (uuid) {
+        return this.lowReadGithub (this.storage + uuid)
+    }
+    this.remove = async function (uuid) {
+        // Get selfCategories
+        var listCategories = await this.lowReadGithub(this.selfCategory + uuid)
+        // Remove Categories
+        for await(let e of listCategories.content) {
+            await this.lowDeleteGithub(this.category + e + "/" + uuid)
+        }
+        // Remove selfCategories
+        await this.lowDeleteGithub(this.selfCategory + uuid)
+        // Remove element
+        await this.lowDeleteGithub(this.storage + uuid)
+        return {status:ok}
     }
 
-    this.lowWriteGithub = async function (filename, obj) {
+    this.getFromCategory = async function ( category ) {
+        return this.lowReadDirGithub(this.category + category)
+    }
+
+    this.lowWriteGithub = async function (filename, reg) {
         const options = this.getOptions(filename)
         this.connectOctokit();
+        var content = (reg) ? Buffer.from(JSON.stringify(reg)).toString('base64') : "";
         var obj = Object.assign(options,{
             message: `writeObject`,
-            content: Buffer.from(JSON.stringify(obj)).toString('base64'),
+            content: content,
         })
 
         var current = await this.lowReadGithub(filename)
@@ -67,8 +98,8 @@ function Ghdb ( config ) {
             return error
         })
     }
-    this.lowReadGithub = async function (filename) {
-        const options = this.getOptions(filename)
+    this.lowReadGithubCall = async function (f) {
+        const options = this.getOptions(f)
         this.connectOctokit();
         let res = null
         try {
@@ -76,10 +107,11 @@ function Ghdb ( config ) {
         } catch(e) {
             return null     
         }
-        if (!res) {
-            return null
-        }
-    
+        return res       
+    }
+    this.lowReadGithub = async function (filename) {
+        var res = await this.lowReadGithubCall(filename)
+        if (!res) return null
         var content = Buffer.from(res.data.content, 'base64').toString()
         try {
             content = JSON.parse(content)
@@ -87,6 +119,13 @@ function Ghdb ( config ) {
         } catch (e) {
             return null
         }
+    }
+    this.lowReadDirGithub = async function (dir) {
+        var res = await this.lowReadGithubCall(dir)
+        const asyncRes = await Promise.all(res.data.map(async (e) => {
+            return {path: e.path, type: e.type};
+        }));
+        return asyncRes
     }
     this.lowDeleteGithub = async function (filename) {
         const options = this.getOptions(filename)
---
+++ test/ghdbCategories.test.js
@@ -0,0 +1,33 @@
+const Ghdb = require("../src/ghdb");
+require('dotenv').config({debug: true});
+var ghdbObj
+var guuid
+
+jest.setTimeout(60000)
+
+beforeAll(()=>{
+    ghdbObj = new Ghdb( { personalAccessToken: process.env.ACCESSTOKEN, 
+        owner: process.env.GH_USER, 
+        repo: process.env.GH_REPOSITORY, 
+        path: process.env.GH_PATH } )
+})
+
+test("Create object", () => {
+    expect(ghdbObj.repo).toBe(process.env.GH_REPOSITORY);
+});
+
+test("Create register", () => {
+    return ghdbObj.create({hello: 'World'}, ['post', 'important'])
+    .then( data => {
+        guuid = data
+        expect(data.length).toBe(40);
+    })
+})
+
+test("Read last register", () =>  {
+    return ghdbObj.read(guuid)
+    .then ( data => {
+        expect(data.content.hello).toBe('World')
+    })
+})
+
