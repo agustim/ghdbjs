72fcee2651f5ae964c0d1f8ae5920b8920aa0df9
Agustí Moll <agusti@gmail.com>
2020-11-07 01:35:43 +0100

    Unit test, because in separate files test does not work.

---
+++ test/ghdbUnic.test.js
@@ -0,0 +1,97 @@
+// Unic test file
+const Ghdb = require("../src/ghdb");
+require('dotenv').config({debug: true});
+var ghdbObj
+var filename
+var envGlobal = { ObjectReg: {hello: 'World'}, ArrCategories: ['post', 'important'] }
+jest.setTimeout(60000)
+
+beforeAll(()=>{
+    ghdbObj = new Ghdb( { personalAccessToken: process.env.ACCESSTOKEN, 
+        owner: process.env.GH_USER, 
+        repo: process.env.GH_REPOSITORY, 
+        path: process.env.GH_PATH } )
+    ghdbObj.generateUID()
+    .then(d => {
+        filename = d + ".data"
+    })
+})
+
+
+test("Generate UID", () => {
+    return ghdbObj.generateUID()
+    .then( data => {
+        expect(data.length).toBe(40);
+    })
+})
+
+test("Write file", () => {
+    return ghdbObj.lowWriteGithub(filename, {hello: 'World'})
+    .then( data => {
+        expect(data.status.toString().substring(0,2)).toBe("20")
+    })
+})
+
+test("Read file", () => {
+    return ghdbObj.lowReadGithub(filename)
+    .then( data => {
+        expect(data.content.hello).toBe('World')
+    })
+})
+
+test("Delete existent file", () => {
+    return ghdbObj.lowDeleteGithub(filename)
+    .then( data => {
+        expect(data.status).toBe(200)
+    })
+})
+
+test("Delete does not file", () => {
+    return ghdbObj.lowDeleteGithub(filename)
+    .then( data => {
+        expect(data).toBe(null)
+    })
+})
+
+
+test("Create register", () => {
+    return ghdbObj.create(envGlobal.ObjectReg, envGlobal.ArrCategories)
+    .then( data => {
+        envGlobal.guuid = data
+        expect(data.length).toBe(40);
+    })
+})
+
+test("Read last register", () =>  {
+    return ghdbObj.read(envGlobal.guuid)
+    .then ( data => {
+        expect(data.content.hello).toBe(envGlobal.ObjectReg.hello)
+    })
+})
+
+test("Read Categories from last register", () => {
+    return ghdbObj.readCategoriesFromUuid(envGlobal.guuid)
+    .then ( data => { 
+        expect(JSON.stringify(data)).toBe(JSON.stringify(envGlobal.ArrCategories))
+    })
+})
+
+test("Read all registres from Category", () => {
+    return ghdbObj.getFromCategory("post")
+    .then( data => {
+        var listContent = data.filter( e => {
+            var parts = e.path.split("/")
+            return (parts[ parts.length - 1 ] === envGlobal.guuid )
+        })
+        expect(listContent.length).toBe(1)
+    })
+})
+
+test("Remove register", () => {
+    return ghdbObj.remove(envGlobal.guuid)
+    .then ( data => (
+        expect(data.status).toBe("ok")
+    ))
+})
+
+
